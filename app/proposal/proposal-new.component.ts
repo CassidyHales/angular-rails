7a6c443d881fa56eff7bfc2542a74c4a4320513f
Cassidy Hales <cassidyhales11@gmail.com>
2017-09-06 11:43:46 -0600

    Created a component that will create new proposals

---
+++ app/proposal/proposal-new.component.ts
@@ -0,0 +1,11 @@
+import { Proposal } from './proposal';
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'proposal-new',
+  templateUrl: 'proposal-new.component.html'
+})
+export class ProposalNewComponent {
+  proposal = new Proposal;
+}
